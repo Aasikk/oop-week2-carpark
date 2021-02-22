7a211ddcaf59e435f935d207899fedd395581666
Julien Matos Falcao <julien.matos-falcao@epita.fr>
2021-02-22 16:20:00 +0100

    last commit

---
+++ Parking Problem2/Parking Problem2/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Parking_Problem2
+{
+    public class Client
+    {
+        public string Client_Name;
+        public int Time_Spent;
+
+        public string ClientName
+        {
+            get => Client_Name;
+            set => Client_Name = value;
+        }
+
+        public int TimeSpent
+        {
+            get => Time_Spent;
+            set => Time_Spent = value;
+        }
+
+        public Client(string _clientName, int _timeSpent)
+        {
+            Client_Name = _clientName;
+            Time_Spent = _timeSpent;
+        }
+
+        public double calculateCharges(int Time_Spent)
+        {
+            double res = 2;
+            if (Time_Spent > 3)
+            {
+                res += (Time_Spent - 3) * 0.5;
+                if (Time_Spent == 24)
+                {
+                    res =10;
+                }
+            }
+            return res;
+        }
+
+        public override string ToString()
+        {
+            return $"Client name: {Client_Name},  Time spent: {Time_Spent} hours";
+        }
+    }
+
+}
---
+++ Parking Problem2/Parking Problem2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Parking_Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client C1 = new Client("Kate", 1);
+            Client C2 = new Client("Elliot", 4);
+            Client C3 = new Client("Leonor", 10);
+            Client C4 = new Client("Michael", 24);
+            
+            Console.WriteLine(C1);
+            Console.WriteLine("Total charge is {0} $", C1.calculateCharges(C1.Time_Spent));
+            Console.WriteLine(C2);
+            Console.WriteLine("Total charge is {0} $",C2.calculateCharges(C2.Time_Spent));
+            Console.WriteLine(C3);
+            Console.WriteLine("Total charge is {0} $", C3.calculateCharges(C3.Time_Spent));
+            Console.WriteLine(C4);
+            Console.WriteLine("Total charge is {0} $", C4.calculateCharges(C4.Time_Spent));
+            
+            
+            
+        }
+    }
+}
